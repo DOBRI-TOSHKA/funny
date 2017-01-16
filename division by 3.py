8de0450bce469f8938b252859f62aeb815e5f0fa
DOBRI-TOSHKA <ya.zubarevanton@yandex.ru>
2017-01-17 00:04:08 +0300

    test division by 3

---
+++ division by 3.py
@@ -0,0 +1,17 @@
+import re
+import sys
+
+# Вам дана последовательность строк.
+# Выведите строки, содержащие двоичную запись числа, кратного 3.
+
+pattern = r"^((1(01*0)*1|0)*)$"
+
+
+for line in sys.stdin:
+    try:
+        test_line = line.rstrip()
+        f = re.findall(pattern, test_line)
+        if f is not [] and f[0][0] == test_line:
+            print(f[0][0])
+    except IndexError:
+        pass
